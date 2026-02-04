1ac456810d26a50b0451db7ebcb6bedd1129ca18
chelsea24wangari <chelsea14wangari@gmail.com>
2026-02-04 08:40:13 +0300

    new project

---
+++ variable.py
@@ -0,0 +1,19 @@
+# Variable is a container used to store a value
+
+school = "eMobilis"
+firstname = "John"
+lastname = "Smith"
+num1 = 45
+num2 = 100
+language = "Python"
+
+print(school)
+print(firstname)
+print(lastname)
+print(firstname, lastname)
+print(school, "is a coding school")
+print(language, "python is a web programming language")
+print(num1+num2)
+
+
+print("the sum of", num1,"and", num2, "is", num1+num2 )
