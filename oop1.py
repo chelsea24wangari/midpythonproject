77246127186fba16713a0210c4876c462659ffb1
samuelmwangi07 <wamarunda2007@gmail.com>
2026-02-10 10:36:44 +0300

    Adding existing file to project

--- oop1.py
+++ oop1.py
@@ -19,6 +19,7 @@ employee2 = Employee("Frank Ocean","Programme Manager","Single",34)
 print(employee2.fullname, employee2.status, employee2.position, employee2.age)
 
 employee3 = Employee("Alice Kimani","Lecturer","Married",56)
+print(employee3.fullname, employee3.status, employee3.position, employee3.age)
 
 
 
