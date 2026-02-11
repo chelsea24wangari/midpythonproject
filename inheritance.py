f067a15206d5023ee4da446c4dfb488e7cb35361
chelsea24wangari <chelsea14wangari@gmail.com>
2026-02-11 10:05:24 +0300

    initial project

--- inheritance.py
+++ inheritance.py
@@ -14,14 +14,14 @@ class Cat:
     def climb(self):
         print("Cat is climbing a tree")
 
-class Horse:
+class Horse(Animal):
     hasTail = True
 
     def neigh(self):
         print("Horse is neighing")
 
 a  = Animal()
-a.
+
 
 
 c  = Cat()
